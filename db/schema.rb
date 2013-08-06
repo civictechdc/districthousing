55d072db2dc2ebce6cfea2560965577659477d9b
Marcus Louie <marcus.louie@gmail.com>
2013-08-06 23:51:32 +0000

    Added db/schema.rb

---
+++ db/schema.rb
@@ -0,0 +1,48 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20130623222643) do
+
+  create_table "applicants", :force => true do |t|
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "middle_name"
+    t.string   "ssn"
+    t.datetime "dob"
+    t.string   "gender"
+    t.string   "res_street_address"
+    t.string   "res_apt"
+    t.string   "res_city"
+    t.string   "res_state"
+    t.string   "res_zip"
+  end
+
+  create_table "residents", :force => true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "middle_name"
+    t.string   "ssn"
+    t.datetime "dob"
+    t.string   "gender"
+    t.string   "res_street_address"
+    t.string   "res_apt"
+    t.string   "res_city"
+    t.string   "res_state"
+    t.string   "res_zip"
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+  end
+
+end
