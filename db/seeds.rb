3f3b00d63a271c0accb2a88ded13fee8a2127c6b
Jeff Runningen <jeffrunningen@gmail.com>
2013-09-02 09:48:17 -0400

    Add a seeds.rb with one resident

---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+Resident.destroy_all
+Resident.create(first_name: "Walter", last_name: "White", middle_name: "Hartwell", res_street_address: "308 Negra Arroyo Lane", res_city: "Albuquerque", res_state: "New Mexico", res_zip: "87122", ssn: "123-45-6789", dob:"7/9/1959", gender:"Male")
