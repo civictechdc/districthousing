a0ec4a4273194ecf271fcfe564f4870ae6317672
Jeff Runningen <jeffrunningen@gmail.com>
2013-10-17 01:20:51 -0400

    Add invalid cart detection
    Just a learning experience for me, really.  From Chapter 10 of Agile Web
    Development with Rails.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -13,11 +13,16 @@ class CartsController < ApplicationController
   # GET /carts/1
   # GET /carts/1.json
   def show
-    @cart = Cart.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @cart }
+    begin
+      @cart = Cart.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      logger.error "Attempt to access an invalid cart #{params[:id]}"
+      redirect_to housing_forms_url, notice: "Invalid cart"
+    else
+      respond_to do |format|
+        format.html # show.html.erb
+        format.json { render json: @cart }
+      end
     end
   end
 
