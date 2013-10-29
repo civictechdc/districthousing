4bbaaf75ad6981b97b5e25f395be9acb77e36ef3
Jeff Runningen <jeffrunningen@gmail.com>
2013-10-29 02:43:25 -0400

    Use _url paths for redirects

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -48,7 +48,7 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to HousingForm }
+        format.html { redirect_to housing_forms_url }
         format.json { render json: @line_item, status: :created, location: @line_item }
       else
         format.html { render action: "new" }
@@ -80,7 +80,7 @@ class LineItemsController < ApplicationController
     @line_item.destroy
 
     respond_to do |format|
-      format.html { redirect_to HousingForm }
+      format.html { redirect_to housing_forms_url }
       format.json { head :no_content }
     end
   end
