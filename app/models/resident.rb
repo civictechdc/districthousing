ddf38cb9bc24c99be377a3950c8f6f46422fd33a
Jeff Runningen <jeffrunningen@gmail.com>
2013-12-31 01:44:01 -0500

    Require dragoman so Resident can actually use it

--- app/models/resident.rb
+++ app/models/resident.rb
@@ -1,3 +1,5 @@
+require 'dragoman'
+
 class Resident < ActiveRecord::Base
   attr_accessible :dob, :first_name, :gender, :last_name, :middle_name, :res_apt, :res_city, :res_state, :res_street_address, :res_zip, :ssn
 
