0c25a6f9fffbdb0555b7bc32efbf9a20ac51d573
Jeff Runningen <jeffrunningen@gmail.com>
2014-01-06 01:39:53 -0500

    Refactor and add features to Dragoman

--- lib/dragoman.rb
+++ lib/dragoman.rb
@@ -1,29 +1,74 @@
 #!/usr/bin/env ruby
 
-require 'ostruct'
 require 'set'
 
 class Dragoman
 
+  def self.is_provided(provider, source_field)
+    provider.respond_to? source_field and not provider.public_send(source_field).to_s.empty?
+  end
+
+  # Indicates that the user requested a field, but the provider doesn't contain
+  # the necessary information to produce the result.  User code might want to
+  # prompt for more information in the provider.
+  class MissingItemsError < StandardError
+    attr_reader :missing_items
+
+    def initialize missing
+      @missing_items = missing
+    end
+  end
+
+  # The Dragoman was asked about a particular field, but it doesn't have any
+  # rules that match the field name.
+  class NoMatchError < StandardError; end
+
   class Rule
-    attr_accessor :pattern, :productions
+    attr_reader :pattern, :productions
 
     def initialize pattern, productions
       @pattern = pattern
       @productions = productions
     end
 
+    def preferred_items
+      parameter_names @productions.first
+    end
+
     def required_items
-      Set.new(required_parameter_names)
+      parameter_names @productions.last
+    end
+
+    def missing_items provider
+      required_items.reject { |param| Dragoman.is_provided(provider, param) }
+    end
+
+    def produce provider
+      @productions.each do |production|
+        if usable? production, provider
+          return invoke production, provider
+        end
+      end
+
+      raise MissingItemsError.new(missing_items(provider))
+    end
+
+    def invoke production, provider
+      # FIXME: Returns a lambda when no provider is set.  Why?
+      arguments = parameter_names(production).map do |param|
+        provider.public_send(param)
+      end
+      return production.call(*arguments).to_s
+    end
+
+    def usable? production, provider
+      parameter_names(production).all? { |param| Dragoman.is_provided(provider, param) }
     end
 
     private
 
-    # FIXME: There might be different subsets of required items, if there is
-    # more than one production.  Change this function's return value to express
-    # that.
-    def required_parameter_names
-      @productions.first.parameters.map { |param| param.last }
+    def parameter_names production
+      production.parameters.map { |param| param.last }
     end
   end
 
@@ -32,11 +77,6 @@ class Dragoman
   def initialize
     @rules = Array.new
     @provider = nil
-
-  end
-
-  def is_provided(source_field)
-    @provider.respond_to? source_field and not @provider.public_send(source_field).to_s.empty?
   end
 
   def learn pattern, *productions
@@ -48,34 +88,17 @@ class Dragoman
   end
 
   def missing_items target
-    Set.new(required_items(target).reject { |param| is_provided(param) })
+    matching_rule(target).missing_items(provider)
   end
 
   def field target
-    matching_rule(target).productions.each do |production|
-      if is_usable production
-        return invoke production
-      end
-    end
+    matching_rule(target).produce(@provider)
   end
 
   private
 
   def matching_rule target
-    @rules.detect { |rule| rule.pattern =~ target }
-  end
-
-  def is_usable production
-    production.parameters.all? { |param| is_provided(param.last) }
-  end
-
-  def invoke production
-    # FIXME: Replace the map with a required_items call
-    # FIXME: Returns a lambda when no provider is set.  Why?
-    arguments = production.parameters.map do |param|
-      @provider.public_send(param.last)
-    end
-    return production.call(*arguments).to_s
+    @rules.detect { |rule| rule.pattern =~ target } or raise NoMatchError, "Unrecognized field \"#{target}\""
   end
 
 end
--- test/unit/dragoman_test.rb
+++ test/unit/dragoman_test.rb
@@ -1,5 +1,6 @@
 require "test/unit"
 
+require "ostruct"
 require "dragoman"
 
 class TestDragoman < Test::Unit::TestCase
@@ -13,10 +14,11 @@ class TestDragoman < Test::Unit::TestCase
     @d.learn(/FullName.*/,
              ->(first_name, middle_name, last_name) { "#{first_name} #{middle_name} #{last_name}"},
              ->(first_name, last_name) { "#{first_name} #{last_name}"})
+    @d.learn(/PhoneNumber/,
+             ->(phone_number) { phone_number } )
   end
 
   def test_produces_field
-
     assert_equal "Walter White",  @d.field("FullName")
     assert_equal "Walter White",  @d.field("FullName1")
 
@@ -25,16 +27,25 @@ class TestDragoman < Test::Unit::TestCase
 
     @provider.middle_name = "Hartwell"
     assert_equal "Walter Hartwell White",  @d.field("FullName")
+  end
 
-    # FIXME: What does Dragoman do in case of no matching rule?  Raise an exception?
+  def test_is_useful_for_unfillable_fields
+    assert_raise Dragoman::NoMatchError do
+      @d.field("No such field")
+    end
+
+    exception = assert_raise Dragoman::MissingItemsError do
+      @d.field("PhoneNumber")
+    end
+
+    assert_equal [:phone_number], exception.missing_items
   end
 
   def test_shows_required_items
-    assert_equal Set.new([:first_name, :middle_name, :last_name]),  @d.required_items("FullName")
+    assert_equal [:first_name, :last_name].to_set, @d.required_items("FullName").to_set
   end
 
   def test_shows_missing_items
-    @d.learn(/FullName.*/, ->(first_name, middle_name, last_name) { "#{first_name} #{middle_name} #{last_name}"})
-    assert_equal Set.new([:middle_name]),  @d.missing_items("FullName")
+    assert_equal [:phone_number],  @d.missing_items("PhoneNumber")
   end
 end
