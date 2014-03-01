c807a963f6de33051f372c72c0da2009a54a1b29
Jeff Runningen <jeffrunningen@gmail.com>
2014-02-28 23:55:01 -0500

    Create join table for forms and field names, and seed it

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140301042142) do
+ActiveRecord::Schema.define(:version => 20140301043156) do
 
   create_table "aliases", :force => true do |t|
     t.string  "name"
@@ -28,6 +28,11 @@ ActiveRecord::Schema.define(:version => 20140301042142) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "form_fields_housing_forms", :id => false, :force => true do |t|
+    t.integer "form_field_id"
+    t.integer "housing_form_id"
   end
 
   create_table "housing_forms", :force => true do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,12 @@
 Resident.destroy_all
 Resident.create(first_name: "Walter", last_name: "White", middle_name: "Hartwell", res_street_address: "308 Negra Arroyo Lane", res_city: "Albuquerque", res_state: "New Mexico", res_zip: "87122", ssn: "123-45-6789", dob:"7/9/1959", gender:"Male")
 
+# Load information from all PDFs in public/forms
 HousingForm.destroy_all
 Dir.glob(Rails.root.join("public/forms/*.pdf")) do |pdf_path|
   pdf_name = File.basename pdf_path
-  HousingForm.create(name: pdf_name, uri: pdf_path)
+  form = HousingForm.create(name: pdf_name, uri: pdf_path)
+  PDF_FORMS.get_field_names(pdf_path).each do |field_name|
+     form.form_fields << FormField.find_or_create_by_name(field_name)
+  end
 end
