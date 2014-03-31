b6ad3f907b32dd0f209a4410f62b10056cfa2c53
Jeff Runningen <jeffrunningen@gmail.com>
2014-03-30 21:08:23 -0400

    Format resident date of birth as mm/dd/yyyy

--- app/models/resident.rb
+++ app/models/resident.rb
@@ -1,11 +1,10 @@
 class Resident < ActiveRecord::Base
-  attr_accessible :dob, :first_name, :gender, :last_name, :middle_name, :res_apt
+  attr_accessible :first_name, :gender, :last_name, :middle_name, :res_apt
   attr_accessible :res_city, :res_state, :res_street_address, :res_zip, :ssn
   attr_accessible :phone, :work_phone, :home_phone, :cell_phone, :preferred_phone
   attr_accessible :citizenship
   attr_accessible :nationality, :email, :race, :student_status, :marital_status
   attr_accessible :mail_city, :mail_state, :mail_street_address, :mail_zip
-
 
   has_many :previous_ssns
 
@@ -46,4 +45,9 @@ class Resident < ActiveRecord::Base
       end
     end.flatten.reject(&:nil?).to_set
   end
+
+  def dob
+    attributes["dob"].strftime("%m/%d/%Y")
+  end
+
 end
