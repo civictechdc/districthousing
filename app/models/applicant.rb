a56f60614de41f3502ab2263cf127d6e13de5420
Jeff Runningen <jeffrunningen@gmail.com>
2014-04-01 18:55:35 -0400

    Delegate Applicant accessors to Person
    Now the form filling appears to work again.

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -1,6 +1,34 @@
 class Applicant < ActiveRecord::Base
   belongs_to :identity, :class_name => "Person", :foreign_key => "self_id"
 
+  delegate :dob,
+    :first_name,
+    :gender,
+    :last_name,
+    :middle_name,
+    :res_apt,
+    :res_city,
+    :res_state,
+    :res_street_address,
+    :res_zip,
+    :ssn,
+    :phone,
+    :work_phone,
+    :home_phone,
+    :cell_phone,
+    :preferred_phone,
+    :citizenship,
+    :nationality,
+    :email,
+    :race,
+    :student_status,
+    :marital_status,
+    :mail_city,
+    :mail_state,
+    :mail_street_address,
+    :mail_zip,
+    to: :identity
+
   def preferred_attrs_for field_names
     field_names.map do |field_name|
       begin
