03e58ae8f390428b7bb9c742b55e6ccec93afa4f
Jeff Runningen <jeffrunningen@gmail.com>
2014-05-14 14:34:57 -0400

    Use secrets.yml
    The edit to application.rb comes from http://stackoverflow.com/a/22130149/303851

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-DchousingApps::Application.config.secret_token = '6a83f7fa4ef963f9a8f042fd5573993b2ecfb0ac23256ad784ec94034466b147ec1f414b7e5e6c70cf10cb3489f4075264f88fab6f8da005d5d1280f70adc7c6'
