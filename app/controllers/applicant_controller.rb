f90b7575a65ec16a33a1d3a36c845ff0b680d33d
Jeff Runningen <jeffrunningen@gmail.com>
2014-06-02 18:42:31 -0400

    Clean up some debugging code

--- app/controllers/applicant_controller.rb
+++ app/controllers/applicant_controller.rb
@@ -4,11 +4,7 @@ class ApplicantController < ApplicationController
     @applicant = Applicant.find(params[:id])
 
     respond_to do |format|
-      if @applicant.update_attributes(params[:applicant])
-        logger.info "It worked!"
-      else
-        logger.info "It failed!"
-      end
+      @applicant.update_attributes(params[:applicant])
       format.html { redirect_to picker_path }
     end
   end
