f3c372fdd30543a92c3998c9f8c78b14d5b4a09e
Jeff Runningen <jeffrunningen@gmail.com>
2014-06-27 13:53:32 -0400

    Make household members editable without applicant nesting
    Household member updates are now handled through HouseholdMember#update, rather
    than indirectly through Applicant#update with nested attributes.

--- app/controllers/household_member_controller.rb
+++ app/controllers/household_member_controller.rb
@@ -1,4 +1,5 @@
 class HouseholdMemberController < ApplicationController
+  before_action :set_household_member, only: [:show, :edit, :update, :destroy]
 
   def new
     current_applicant.household_members << HouseholdMember.create do |hh|
@@ -13,4 +14,21 @@ class HouseholdMemberController < ApplicationController
     end
   end
 
+  def update
+    if @household_member.update(household_member_params)
+      redirect_to form_path, notice: 'Housing form was successfully updated.'
+    else
+      redirect_to form_path, notice: 'Housing form could not be updated.'
+    end
+  end
+
+  private
+
+  def set_household_member
+    @household_member = HouseholdMember.find(params[:id])
+  end
+
+  def household_member_params
+    params[:household_member]
+  end
 end
