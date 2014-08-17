311c852e997e434e95a27cb3ae3f948a683a7e9e
Michael Burkman <michael.burkman@gmail.com>
2014-08-17 11:55:31 -0400

    Add a VagrantFile and Puppet config for easy and consistent onboarding, no matter what OS the dev is using. Also creates a nice sandbox environment, as well as making it easier to upgrade dependency versions.
    Download Vagrant and Oracle VirtualBox (it works with some other VMs supposedly but I've only tested on VB)
    
    Navigate to the dchousing-apps root folder, and do `vagrant up`
    This will create the VM, install ruby, rails, sqlite, memcache, and all the gems. It takes a few minutes. Go get some coffee.
    Once the VM is up, do `vagrant ssh` to connect to the new VM, which will act as your Rails server.
    Navigate to /vagrant (`cd /vagrant`), which is where the dchousing-apps files will be. This directory is shared betwee the VM and your host machine.
    From here, set up the DB like you normally would: `rake db:setup`. (I'd like to automate this step as well in the Puppet config, but it's getting test errors.)
    Now the server is ready to go! Do `rails server` and it should start.
    Open up a browser on your host machine (not the VM), and go to http://localhost:3000 to access the app.
    Edit files on your host machine the same way you normally would. The VM is just for running Rails and the DB.
    
    To get out of the VM, press Ctrl+C to stop the server if it's running, and do `exit` to end the SSH session.
    Do `vagrant halt` to stop your VM. Doing `vagrant up` should restart the VM.
    
    You can also destroy the VM, along with all the Ruby and DB stuff you've downloaded, by doing `vagrant destroy`.
    
    Note: You can destroy and rebuild the VM without messing up any of your files you've checked out!

---
+++ puppet/manifests/default.pp
@@ -0,0 +1,127 @@
+$ar_databases = ['activerecord_unittest', 'activerecord_unittest2']
+$as_vagrant   = 'sudo -u vagrant -H bash -l -c'
+$home         = '/home/vagrant'
+
+# Pick a Ruby version modern enough, that works in the currently supported Rails
+# versions, and for which RVM provides binaries.
+$ruby_version = '2.1.2'
+
+Exec {
+  path => ['/usr/sbin', '/usr/bin', '/sbin', '/bin']
+}
+
+# --- Preinstall Stage ---------------------------------------------------------
+
+stage { 'preinstall':
+  before => Stage['main']
+}
+
+class apt_get_update {
+  exec { 'apt-get -y update':
+    unless => "test -e ${home}/.rvm"
+  }
+}
+class { 'apt_get_update':
+  stage => preinstall
+}
+
+# --- SQLite -------------------------------------------------------------------
+
+package { ['sqlite3', 'libsqlite3-dev']:
+  ensure => installed;
+}
+
+# --- Memcached ----------------------------------------------------------------
+
+class { 'memcached': }
+
+# --- Packages -----------------------------------------------------------------
+
+package { 'curl':
+  ensure => installed
+}
+
+package { 'build-essential':
+  ensure => installed
+}
+
+package { 'git-core':
+  ensure => installed
+}
+
+# Nokogiri dependencies.
+package { ['libxml2', 'libxml2-dev', 'libxslt1-dev']:
+  ensure => installed
+}
+
+# ExecJS runtime.
+package { 'nodejs':
+  ensure => installed
+}
+
+# --- Ruby ---------------------------------------------------------------------
+
+exec { 'install_rvm':
+  command => "${as_vagrant} 'curl -L https://get.rvm.io | bash -s stable'",
+  creates => "${home}/.rvm/bin/rvm",
+  require => Package['curl']
+}
+
+exec { 'install_ruby':
+  # We run the rvm executable directly because the shell function assumes an
+  # interactive environment, in particular to display messages or ask questions.
+  # The rvm executable is more suitable for automated installs.
+  #
+  # use a ruby patch level known to have a binary
+  command => "${as_vagrant} '${home}/.rvm/bin/rvm install ruby-${ruby_version} --binary --autolibs=enabled && rvm alias create default ${ruby_version}'",
+  creates => "${home}/.rvm/bin/ruby",
+  require => Exec['install_rvm']
+}
+
+# RVM installs a version of bundler, but for edge Rails we want the most recent one.
+exec { "install_bundler": 
+  command => "${as_vagrant} 'gem install bundler --no-rdoc --no-ri'",
+  creates => "${home}/.rvm/bin/bundle",
+  require => Exec['install_ruby']
+}
+
+# --- Locale -------------------------------------------------------------------
+
+# Needed for docs generation.
+exec { 'update-locale':
+  command => 'update-locale LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8'
+}
+
+# --- For DC Housing Apps Specifically -----------------------------------------
+
+$rubyhome = "${home}/.rvm/gems/ruby-${ruby_version}/bin"
+
+stage { 'dc-housing': }
+
+class dc-housing-setup { 
+  package { "pdftk":
+    ensure => latest
+  }
+
+  exec { "run_bundle_install":
+    command => "${as_vagrant} ${rubyhome}/bundle install",
+    cwd => "/vagrant",
+    logoutput => true,
+    timeout => 1800 # Bundle install takes a while
+  }
+
+  # Can't quite get this to work, so you'll have to do it manually the first time you log in to the server
+  #exec { "rake_setup_db":
+  #  command => "${as_vagrant} ${rubyhome}/rake db:setup",
+  #  cwd => "/vagrant",
+  #  logoutput => true,
+  #  require => Exec['run_bundle_install']
+  #}
+}
+
+class { 'dc-housing-setup':
+  stage => dc-housing
+}
+
+# Make sure dc-housing stuff happens after everything else
+Stage['main'] -> Stage['dc-housing']
