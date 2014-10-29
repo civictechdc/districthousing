eb048f5f0a800e08148bc3f123b098362c38cabe
Jeff Runningen <jeffrunningen@gmail.com>
2014-10-28 22:54:48 -0400

    Don't require auth for the main page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+  skip_before_filter :authenticate_user!
 
   def index
     @no_sidebar = true
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -3,6 +3,10 @@ require "test_helper"
 class HomeControllerTest < ActionController::TestCase
   include Devise::TestHelpers
 
+  def test_index
+    get :index
+  end
+
   def test_onboarding
     sign_in users(:one)
     get :onboarding
