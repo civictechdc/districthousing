da99e1e34839ef306dd4d48cb2382709a3a0d41e
Jeff Runningen <jeffrunningen@gmail.com>
2014-10-30 23:50:11 -0400

    Keep the navbar on the front page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,6 @@ class HomeController < ApplicationController
   skip_before_filter :authenticate_user!
 
   def index
-    @no_sidebar = true
-    @landing = true
-
     if current_user
       redirect_to onboarding_path
     end
