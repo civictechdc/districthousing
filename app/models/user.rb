d1a13fd43f75d23cde97bff21f550d6d2331effa
Jeff Runningen <jeffrunningen@gmail.com>
2014-11-16 21:45:14 -0500

    Remove attr_accessible from User and remove protected_attributes gem
    It's almost like we're running Rails 4!

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-
   has_many :applicants
 
   USER_ROLES = {
