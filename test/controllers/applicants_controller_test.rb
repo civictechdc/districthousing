6c555fd13923d5f68e294d15a37c3a8c9a3b8bc1
Jeff Runningen <jeffrunningen@gmail.com>
2014-12-01 20:48:44 -0500

    Add number questions to the new applicant page
    Fixes #151, I think.

--- test/controllers/applicants_controller_test.rb
+++ test/controllers/applicants_controller_test.rb
@@ -13,18 +13,26 @@ class ApplicantsControllerTest < ActionController::TestCase
     sign_in users(:one)
     post :create, {
       first_name: "test_first_name",
-      last_name: "test_last_name"
+      last_name: "test_last_name",
+      household_member_count: 4,
+      residence_count: 5,
+      income_count: 6,
+      employment_count: 7
     }
 
     created_applicant = Applicant.joins(:identity).
       where(
         people: {
           first_name: "test_first_name",
-          last_name: "test_last_name" })
+          last_name: "test_last_name" }).first
 
-    assert_redirected_to created_applicant.first
+    assert_redirected_to edit_person_path(created_applicant.identity)
 
-    assert_equal users(:one).id, created_applicant.first.user_id
+    assert_equal users(:one).id, created_applicant.user_id
+    assert_equal 4, created_applicant.household_members.count
+    assert_equal 5, created_applicant.residences.count
+    assert_equal 6, created_applicant.incomes.count
+    assert_equal 7, created_applicant.employments.count
   end
 
   def test_show
