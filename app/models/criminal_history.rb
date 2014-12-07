d023f5b48b8f076dd3433b13121bd007b3e2c55d
Jeff Runningen <jeffrunningen@gmail.com>
2014-12-07 14:39:50 -0500

    Fix CriminalHistory#to_s for nil year

--- app/models/criminal_history.rb
+++ app/models/criminal_history.rb
@@ -5,6 +5,10 @@ class CriminalHistory < ActiveRecord::Base
   belongs_to :person
 
   def to_s
-    "#{crime_type.label} in #{year.year}"
+    unless year.nil?
+      "#{crime_type.label} in #{year.year}"
+    else
+      "#{crime_type.label}"
+    end
   end
 end
