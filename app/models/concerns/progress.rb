a3f68509c86dee56d80478574394993366b01c6b
Jeff Runningen <jeffrunningen@gmail.com>
2014-12-07 13:54:05 -0500

    Rename Progress methods for consistency

--- app/models/concerns/progress.rb
+++ app/models/concerns/progress.rb
@@ -1,18 +1,18 @@
 module Progress
   extend ActiveSupport::Concern
 
-  def completeness
+  def progress
     filled_field_count * 100 / field_count
   end
 
   def field_count
     fillable_fields.count +
-      sub_models.reduce(0) { |sum, m| sum + send(m).field_count }
+      progress_includes.reduce(0) { |sum, m| sum + send(m).field_count }
   end
 
   def filled_field_count
     fillable_fields.reject { |f| send(f).blank? }.count +
-      sub_models.reduce(0) { |sum, m| sum + send(m).filled_field_count }
+      progress_includes.reduce(0) { |sum, m| sum + send(m).filled_field_count }
   end
 
   def fillable_fields
