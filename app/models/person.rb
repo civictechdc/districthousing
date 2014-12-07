181721dacffd1088c9a03b1528683fde83ff9c0b
Jeff Runningen <jeffrunningen@gmail.com>
2014-12-07 13:11:42 -0500

    Add basic progress bar for identity

--- app/models/person.rb
+++ app/models/person.rb
@@ -199,4 +199,21 @@ class Person < ActiveRecord::Base
     end
   end
 
+  def completeness
+    filled_field_count * 100 / field_count
+  end
+
+  def field_count
+    fillable_fields.count
+  end
+
+  def filled_field_count
+    fillable_fields.reject { |f| send(f).blank? }.count
+  end
+
+  def fillable_fields
+    attribute_names.reject { |a|
+      %w(created_at updated_at applicant_id id address_id).include? a
+    }
+  end
 end
