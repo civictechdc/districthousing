3ccf71410bae859d9c05d7f76bff1cab6a08469e
Jeff Runningen <jeffrunningen@gmail.com>
2015-01-05 21:55:03 -0500

    Reorganize routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,23 @@
 DchousingApps::Application.routes.draw do
   get "/salesforce_applicants/sync", to: "salesforce_applicants#sync_all"
   get "/salesforce_applicants/:id/sync", to: "salesforce_applicants#sync"
-  resources :salesforce_applicants
-
-  resources :criminal_histories
-
-  resources :employments
-
-  resources :residences
-  resources :incomes
 
   devise_for :users
 
   resources :applicants
+  resources :criminal_histories
+  resources :employments
   resources :household_members
-  resources :landlords
   resources :housing_forms
-  get '/download/:id', to: 'housing_forms#download', as: 'download_housing_form'
-
+  resources :incomes
+  resources :landlords
   resources :people
+  resources :residences
+  resources :salesforce_applicants
 
+  get '/download/:id', to: 'housing_forms#download', as: 'download_housing_form'
   get "home/index"
   get "/about", to: "home#about"
-
   get "/pdf_guide", to: "pdf_guide#index"
   get "/dictionary", to: "dictionary#index"
 
