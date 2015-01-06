373b7b780d17c58425cba7cfc6580f9f1510d393
Jeff Runningen <jeffrunningen@gmail.com>
2015-01-06 01:21:20 -0500

    Remove irrelevant test

--- test/unit/salesforce_test.rb
+++
@@ -1,6 +0,0 @@
-class SalesforceApplicantTest < ActiveSupport::TestCase
-  test "mirrors records in salesforce" do
-    assert_equal 3, Applicant.count
-    #SalesforceApplicant.pull_from_salesforce
-  end
-end
