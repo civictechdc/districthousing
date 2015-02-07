82edb40540c844ef47b071591da1c4bf7c0d260a
Jeff Runningen <jeffrunningen@gmail.com>
2015-02-07 13:02:20 -0500

    Re-initialize form fields on seeds
    Fields should get initialized when uploaded or added to the system.
    However, with all the updates we've been making to the seeds, some forms
    on on my workstation have a PDF, but no fields are scanned.  It should
    be kind of a one-time thing, but for convenience, the seeds file will
    re-normalize this.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@ require 'open-uri'
 # exist in the database.  PDFs that are already in the database will be
 # untouched, but PDFs not in the database will be added and given a name based
 # on their filename.
-FormField.delete_all
 
 def download_pdf uri, name
   # Download the file to the public/forms/ directory, and generate a path
@@ -45,6 +44,12 @@ HousingForm.transaction do
       end
     end
   end
+end
+
+# Re-initialize all fields from their PDFs
+FormField.delete_all
+HousingForm.all.each do |h|
+  h.initialize_from_disk!
 end
 
 # Now, make a bunch of fake applicants.
