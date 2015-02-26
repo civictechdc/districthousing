6aa77434f93b9b989f07f7f155a3a85713df1b92
evonfriedland <emanuelfeld@gmail.com>
2015-02-26 04:42:44 +0000

    Alters criminal history test
    Now uses just year, not full date

--- test/unit/field_filling_test.rb
+++ test/unit/field_filling_test.rb
@@ -140,7 +140,7 @@ class FieldFillingTest < ActiveSupport::TestCase
   test "fills criminal history information" do
     assert_equal "Felony", @one.field("Crime1Type")
     assert_equal "MyString", @one.field("Crime1Description")
-    assert_equal "2014-11-17", @one.field("Crime2Date")
+    assert_equal "2014", @one.field("Crime2Date")
     assert_equal "One", @one.field("Crime2FirstName")
     assert_equal "Felony", @one.field("CrimeType")
   end
