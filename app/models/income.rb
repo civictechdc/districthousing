0c9c705d4288262ae1ed11406ea6c33e6c1c7c1f
evonfriedland <emanuelfeld@gmail.com>
2015-02-26 01:30:53 +0000

    Adds IncomeAmount{Interval} fields
    Fixes #246

--- app/models/income.rb
+++ app/models/income.rb
@@ -23,12 +23,48 @@ class Income < ActiveRecord::Base
     Constants::IncomeType.new(income_type).name_form
   end
   
+  def amount_yearly
+    if interval=="weekly"
+      amount.to_i*52
+    elsif interval=="monthly"
+      amount.to_i*12
+    else 
+      amount.to_i
+    end
+  end
+  
+  def amount_monthly
+    if interval=="weekly"
+      amount.to_i*4
+    elsif interval=="yearly"
+      amount.to_i/12.0
+    else 
+      amount.to_i
+    end
+  end
+
+  def amount_weekly
+    if interval=="monthly"
+      amount.to_i/4.0
+    elsif interval=="yearly"
+      amount.to_i/52.0
+    else 
+      amount.to_i
+    end
+  end
+  
   def value_for_field field_name
     case field_name
     when "Source"
       income_type
-    when "Amount"
+    when /^Amount$/
       amount.to_i
+    when "AmountWeekly"
+      amount_weekly
+    when "AmountMonthly"
+      amount_monthly
+    when "AmountYearly"
+      amount_yearly
     when "Interval"
       interval
     when /^Earner(\D+)$/
